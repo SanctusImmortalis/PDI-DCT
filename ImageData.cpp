8cd3766a9cf3dd720e9250a0ee4ebf0616fca39a
SanctusImmortalis <nelson_guedes5@hotmail.com>
2018-04-29 12:06:57 -0300

    RGBImageData

--- ImageData.cpp
+++ ImageData.cpp
@@ -1,1 +1,52 @@
+#include <cstdlib>
 #include "include/ImageData.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
+
+RGBImageData::RGBImageData(char const* filename){
+  this->rawData = stbi_load(filename, &(this->width), &(this->height), &(this->nOfChannels), 3);
+}
+
+RGBImageData::RGBImageData(int x, int y){
+  if(x>=0 && y>=0){
+    this->width = x;
+    this->height = y;
+    this->nOfChannels = 3;
+    this->rawData = (unsigned char*) calloc(3*x*y, 1);
+  }else{
+    this->width = -1;
+    this->height = -1;
+    this->nOfChannels = 0;
+    this->rawData = NULL;
+  }
+}
+
+RGBImageData::~RGBImageData(){
+  stbi_image_free(this->rawData);
+}
+
+RGBPixel RGBImageData::getPixel(int x, int y){
+  RGBPixel pix;
+  if(isOutOfBounds(x, y)){
+    pix.r = pix.g = pix.b = 0;
+  }else{
+    int coord = (x + y*(this->width))*3;
+    unsigned char* arr = this->rawData;
+    pix.r = arr[coord];
+    pix.g = arr[coord + 1];
+    pix.b = arr[coord + 2];
+  }
+  return pix;
+}
+
+bool RGBImageData::setPixel(RGBPixel pixel, int x, int y){
+  if(isOutOfBounds(x, y)){
+    return false;
+  }
+  int coord = (x + y*(this->width))*3;
+  unsigned char* arr = this->rawData;
+  arr[coord] = pixel.r;
+  arr[coord + 1] = pixel.g;
+  arr[coord + 2] = pixel.b;
+  return true;
+}
--- include/ImageData.h
+++ include/ImageData.h
@@ -3,10 +3,21 @@
 
 #include "PipelineData.h"
 
+typedef struct{
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+} RGBPixel;
+
 class RGBImageData : public PipelineData<unsigned char*>{
 using PipelineData<unsigned char*>::PipelineData;
 public:
   RGBImageData(char const* filename);
+  RGBImageData(int x, int y);
+  ~RGBImageData();
+  RGBPixel getPixel(int x, int y);
+  bool setPixel(RGBPixel pixel, int x, int y);
+  bool isOutOfBounds(int x, int y) {return (x<0)||(y<0)||(x>=(this->width))||(y>=(this->height));}
 protected:
   int width, height, nOfChannels;
 };
@@ -15,6 +26,9 @@ class YImageData : public PipelineData<double*>{
 using PipelineData<double*>::PipelineData;
 public:
   YImageData(RGBImageData* rgbImage);
+  double getPixel(int x, int y);
+  bool setPixel(double pixel, int x, int y);
+  bool isOutOfBounds(int x, int y) {return (x<0)||(y<0)||(x>=(this->width))||(y>=(this->height));}
 protected:
   int width, height;
 };
