c7a2a5e21eee563f15c68d4b23bb8a9fa167b266
SanctusImmortalis <nelson_guedes5@hotmail.com>
2018-05-01 19:24:22 -0300

    Convolution step

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include "include/EigenLab.h"
+
 int main(){
+  EigenLab::Parser<Eigen::MatrixXd> parserXd;
+  EigenLab::Value<Eigen::MatrixXd> resultXd;
+  std::string expr;
+
+  expr = "[[1,2];[3,4]]";
+  resultXd = parserXd.eval(expr);
+  int i = 0;
   return 0;
 }
